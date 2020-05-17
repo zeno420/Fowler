8b6943f4287e061c30d71d544a09ba88aed7d24a
Zeno Berkhan <zenoberkhan@rocketmail.com>
2020-05-17 17:14:16 +0200

    Extract Method: Refactor>Extract Method IntelliJ function
    Customer.getFrequentRenterPoints()

--- src/Customer.java
+++ src/Customer.java
@@ -25,11 +25,7 @@ class Customer {
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
             double thisAmount = each.getCharge();
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
+            frequentRenterPoints += getFrequentRenterPoints(each);
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
@@ -38,5 +34,14 @@ class Customer {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+    }
+
+    private int getFrequentRenterPoints(Rental each) {
+        // set frequent renter point
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
+            frequentRenterPoints ++;
+        return frequentRenterPoints;
     }
 }
